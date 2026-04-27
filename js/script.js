3b008a117e0438fdaccad2db658eebf2c24465d1
Douglas Soares <douglassoares.cinema@gmail.com>
2026-04-27 13:42:25 -0300

    feat: adicionando via cep

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 import { FormDinamico } from './FormDinamico.js';
 import { ValidadorForm } from './VailidadorForm.js';
+import { Endereco } from './Endereco.js';
 
 document.addEventListener('DOMContentLoaded', () => {     
     const formDinamico = new FormDinamico('formLogin', 'formCadastro', 'semCadastro', 'comCadastro');
-    const validadorForm = new ValidadorForm('formCadastro', 'cd_senha', 'cd_confirmacao_senha', 'cadastrar', 'msg-erro');
+    const validadorForm = new ValidadorForm('formCadastro', 'cd_senha', 'cd_confirmacao_senha', 'cadastrar', 'erro-senhas');
+    const endereco = new Endereco('cd_cep', 'sg_uf', 'nm_cidade', 'nm_bairro', 'nm_logradouro', 'erro-cep');
 
     formDinamico.init();
     validadorForm.init();
+    endereco.init();
 });
