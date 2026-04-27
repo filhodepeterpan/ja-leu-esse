89d0f77affff08aee4e7a1f918c07cad75f516de
Douglas Soares <douglassoares.cinema@gmail.com>
2026-04-26 21:51:02 -0300

    feat: cria formulario de cadastro correspondente ao banco de dados

--- php/pages/login.php
+++ php/pages/login.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
-    
-require('../scripts/functions.php');
 
-if(verificaLogin()){
+require('../scripts/functions.php');
+require('../scripts/atributos.php');
+
+if (verificaLogin()) {
     header("Location: home.php");
     exit();
 }
@@ -20,25 +21,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $_SESSION['user'] = $email;
             header("Location:../pages/home.php");
             exit();
-        } 
-        else {
+        } else {
             echo "<script>alert('Credenciais inválidas!');</script>";
         }
-    } 
-    else if ($postType === 'cadastro') {
+    } else if ($postType === 'cadastro') {
         $nome = $_POST['nm_usuario'];
         $email = $_POST['nm_email'];
         $senha = $_POST['cd_senha'];
 
         // lógica do banco
 
-        echo "<script>alert('Cadastro realizado com sucesso!');</script>";  
-    }
-    else if (!isset($postType)) {
+        echo "<script>alert('Cadastro realizado com sucesso!');</script>";
+    } else if (!isset($postType)) {
         header("Location: login.php");
         exit();
-    }
-    else{
+    } else {
         echo throw new Exception("Ocorreu um erro inesperado. Tente novamente mais tarde.");
     }
 }
@@ -58,28 +55,40 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <main>
         <div id="formularios">
             <form action="#" method="POST" id="formLogin">
-                <h1>Login</h1>
-                <label for="nm_email">Email:</label>
-                <input type="email" id="nm_email" name="nm_email" required>
-                
-                <label for="cd_senha">Senha:</label>
-                <input type="password" id="cd_senha" name="cd_senha" required>
-
+                <h1 class="form-name">Login</h1>
+                <div class="form-item">
+                    <label for="nm_email">Email:</label>
+                    <input type="email" id="nm_email" name="nm_email" required>
+                </div>
+                <div class="form-item">
+                    <label for="cd_senha">Senha:</label>
+                    <input type="password" id="cd_senha" name="cd_senha" required>
+                </div>
                 <input type="hidden" name="post-type" value="login">
-
-                <button type="submit" class="form-submit">Login</button>
-                <span id="semCadastro" class="verificacao-cadastro">Ainda não possuo cadastro</span>
+                <div class="form-item">
+                    <button type="submit" class="form-submit">Login</button>
+                    <span id="semCadastro" class="verificacao-cadastro">Ainda não possuo cadastro</span>
+                </div>
             </form>
             <form action="#" method="POST" id="formCadastro">
-                <h1>Cadastro</h1>
-                <label for="nm_usuario">Nome:</label>
-                <input type="text" id="nm_usuario" name="nm_usuario" required>
-
-                <label for="nm_email">Email:</label>
-                <input type="email" id="nm_email" name="nm_email" required>
-                
-                <label for="cd_senha">Senha:</label>
-                <input type="password" id="cd_senha" name="cd_senha" required>
+                <h1 class="form-name">Cadastro</h1>
+                <?php foreach ($atributos as $atributo): ?>
+                    <div class="form-item">
+                        <label for="<?= $atributo['id'] ?>"><?= $atributo['nome'] ?></label>
+                        <?php if ($atributo['tipo'] === 'radio'): ?>
+                            <?php foreach ($atributo['options'] as $optionName => $optionValue): ?>
+                                <div class="radio">
+                                    <input type="radio" id="<?= $atributo['id'] . '_' . $optionValue ?>" name="<?= $atributo['id'] ?>"
+                                        value="<?= $optionValue ?>" <?= $atributo['constraints'] ?>>
+                                    <label for="<?= $atributo['id'] . '_' . $optionValue ?>"><?= $optionName ?></label>
+                                </div>
+                            <?php endforeach; ?>
+                        <?php else: ?>
+                            <input type="<?= $atributo['tipo'] ?>" id="<?= $atributo['id'] ?>" name="<?= $atributo['id'] ?>"
+                                <?= $atributo['constraints'] ?>>
+                        <?php endif; ?>
+                    </div>
+                <?php endforeach; ?>
 
                 <input type="hidden" name="post-type" value="cadastro">
 
