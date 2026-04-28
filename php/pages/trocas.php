d2a0706b40e8943df4a80ea3e1b2e1b32ae43ddd
Douglas Soares <douglassoares.cinema@gmail.com>
2026-04-28 18:50:47 -0300

    feat: cria pagina de trocas

--- php/pages/trocas.php
+++ php/pages/trocas.php
@@ -4,4 +4,23 @@
     require('../scripts/functions.php');
     aplicaRestricao();
     
+?><?php
+    session_start();
+    
+    require('../scripts/functions.php');
+    aplicaRestricao();
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php include('../partials/head.php'); ?>
+    <title>Já leu esse? | Trocas</title>
+</head>
+<body>
+    <?php include('../layouts/header.php'); ?>
+    <main></main>
+    <?php include('../layouts/footer.php'); ?>
+</body>
+</html>
