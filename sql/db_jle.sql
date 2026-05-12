964b1474c866a029d4893a90c0f0929400cef64c
Douglas Soares <douglassoares.cinema@gmail.com>
2026-05-12 17:35:22 -0300

    fix: atualiza o sql

--- sql/db_jle.sql
+++ sql/db_jle.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 09/05/2026 às 04:05
+-- Tempo de geração: 12/05/2026 às 22:30
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
